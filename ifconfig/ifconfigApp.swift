f3a9f88c361b0cc691765d6878b93ac0ad8799f9
Eliseo Martelli <me@eliseomartelli.it>
2020-11-15 08:49:21 +0100

    Initial Commit

---
+++ ifconfig/ifconfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  ifconfigApp.swift
+//  ifconfig
+//
+//  Created by Eliseo Martelli on 15/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ifconfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
