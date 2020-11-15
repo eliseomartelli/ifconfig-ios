87e679e29e2a81f67ab194ddf0259b9076982351
Eliseo Martelli <me@eliseomartelli.it>
2020-11-15 10:21:11 +0100

    fix: add viewmodels to App

--- ifconfig/ContentView.swift
+++ ifconfig/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selection: String = "ifconfig"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            IfconfigView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "chart.bar")
+                        Text("Summary")
+                    }
+                }
+                .tag("ifconfig")
+            PortView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Ports")
+                    }
+                }
+                .tag("ports")
+        }
     }
 }
 
--- ifconfig/ifconfigApp.swift
+++ ifconfig/ifconfigApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct ifconfigApp: App {
+    
+    @StateObject var ifconfig = IfconfigViewModel()
+    @StateObject var port = PortViewModel()
+
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(ifconfig)
+                .environmentObject(port)
+                .onAppear {
+                    ifconfig.getIfconfig()
+                }
         }
     }
 }
