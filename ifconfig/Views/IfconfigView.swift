2d0e904258c4f4bd0c1e956e1afece79d04d0481
Eliseo Martelli <me@eliseomartelli.it>
2020-11-15 10:20:42 +0100

    feat: add views

--- ifconfig/Views/IfconfigView.swift
+++ ifconfig/Views/IfconfigView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct IfconfigView: View {
+    
+    @EnvironmentObject var ifconfig: IfconfigViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                if let data = ifconfig.ifconfig {
+                    ScrollView {
+                        VStack {
+                            Info(description: "IP", data: "\(data.ip)")
+                            Info(description: "Country", data: "\(data.country)")
+                            Info(description: "Region", data: "\(data.region_name)")
+                            Info(description: "ZIP Code", data: "\(data.zip_code)")
+                            Info(description: "City", data: "\(data.city)")
+                            Info(description: "Time Zone", data: "\(data.time_zone)")
+                        }
+                    }
+                } else {
+                    ProgressView()
+                }
+            }
+            .navigationBarTitle("Ifconfig")
+        }
     }
 }
 
 struct IfconfigView_Previews: PreviewProvider {
     static var previews: some View {
         IfconfigView()
+    }
+}
+
+struct Info: View {
+    let description: String
+    let data: String
+    var body: some View {
+        HStack {
+            Text("\(description)")
+            Spacer()
+            Text("\(data)")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        }
+        .padding()
     }
 }
--- ifconfig/Views/PortView.swift
+++ ifconfig/Views/PortView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct PortView: View {
+    @State var text = ""
+    @EnvironmentObject var portViewModel: PortViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                SearchBar(text: $text)
+                Button("Check") {
+                    portViewModel.getPort(port: Int(text) ?? 80)
+                }
+            }.padding()
+            Spacer()
+            if let data = portViewModel.port {
+                Text("Port \(data.port) is " + (data.reachable ? "open" : "closed"))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            }
+            Spacer()
+        }
     }
 }
 
 struct PortView_Previews: PreviewProvider {
     static var previews: some View {
         PortView()
+    }
+}
+
+struct SearchBar: View {
+    @Binding var text: String
+
+    var body: some View {
+        TextField("Port (Example: 80)", text: $text)
+            .keyboardType(.numberPad)
+            .padding()
+            .background(Color(.tertiarySystemFill))
+            .cornerRadius(8)
     }
 }
